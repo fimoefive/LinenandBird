ac8355fcca256e13c34345fd0c7d2ab2c07655d0
Martin Sisk <Brothermj@hotmail.com>
2021-09-07 18:37:34 -0500

    added Add(newBird) methods in BirdRepository and BirdController.cs files.

--- LinenandBird/Controllers/BirdController.cs
+++ LinenandBird/Controllers/BirdController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LinenandBird.DataAccess;
+using LinenandBird.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,8 +13,23 @@ namespace LinenandBird.Controllers
   [ApiController]
   public class BirdController : ControllerBase
   {
+    BirdRepository _repo;
 
+    public BirdController()
+    {
+      _repo = new BirdRepository();
+    }
 
+    [HttpGet]
+      public IEnumerable<Bird> GetAllBirds()
+    {
+      return _repo.GetAll();
+    }
+
+    public void AddBird(Bird newBird)
+    {
+      _repo.Add(newBird);
+    }
 
   }
 }
--- LinenandBird/DataAccess/BirdRepository.cs
+++ LinenandBird/DataAccess/BirdRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinenandBird.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +8,26 @@ namespace LinenandBird.DataAccess
 {
   public class BirdRepository
   {
+    static List<Bird> _birds = new List<Bird>
+    {
+      new Bird
+      {
+       Name = "Jimmy",
+       Color = "Red",
+       Size = "Small",
+       Type = BirdType.Dead,
+       Accessories = new List<string> {"Beanie", "Gold Wing Tips"}
+      }
+    };
 
+    internal IEnumerable<Bird> GetAll()
+    {
+    return _birds;
+    }
 
-
+    internal void Add(Bird newBird)
+    {
+      _birds.Add(newBird);
+    }
   }
 }
