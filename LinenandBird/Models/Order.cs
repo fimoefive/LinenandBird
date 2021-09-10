919e8aeb91412d6055199c2c1d68ef0dd46a1f63
Martin Sisk <Brothermj@hotmail.com>
2021-09-10 14:59:13 -0500

    added Order.cs Class in Models and OrdersRepository.cs in DataAccess. Also added an OrderControler in the Controllers folder.

---
+++ LinenandBird/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenandBird.Models
+{
+  public class Order
+  {
+    public Bird Bird { get; set; }
+    public Hat Hat { get; set; }
+    public double Price { get; set; }
+  }
+}
