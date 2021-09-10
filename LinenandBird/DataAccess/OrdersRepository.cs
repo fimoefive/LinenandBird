645c231d38f3fed29452a17429ad54e6c6273a9d
Martin Sisk <Brothermj@hotmail.com>
2021-09-10 16:29:43 -0500

    Generated a Add Order Method in the OrdersRepository for order.Id = NewGuid().

--- LinenandBird/DataAccess/OrdersRepository.cs
+++ LinenandBird/DataAccess/OrdersRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinenandBird.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace LinenandBird.DataAccess
 {
   public class OrdersRepository
   {
+    static List<Order> _orders = new List<Order>();
+
+    internal void Add(Order order)
+    {
+      order.Id = Guid.NewGuid();
+
+      _orders.Add(order);
+    }
   }
 }
